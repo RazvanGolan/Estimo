e865a3d1745a1376dcac7bb6a5c152bd9791a79a
RazvanGolan <razvanbitea@gmail.com>
2025-09-23 12:03:24 +0300

    added vercel observability

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import ClientRouter from "./components/ClientRouter"
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import "./App.css"
 
 function App() {
-  return <ClientRouter />
+  return (
+    <>
+      <ClientRouter />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  )
 }
 
 export default App
